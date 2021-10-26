9212e4beaac355c05591f1e3890fab2c050d103f
Keaton Burleson <keaton.burleson@me.com>
2021-10-26 11:32:55 -0500

    Forgot to push/add billing component

---
+++ src/app/modules/client/client-pages/client-billing/client-billing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-billing',
+  templateUrl: './client-billing.component.html',
+  styleUrls: ['./client-billing.component.scss']
+})
+export class ClientBillingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
